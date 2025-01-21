988ddce64acb3a1e4e9f782e1a415617a73418a2
Коммерческие курсы <comcuru@teach.local>
2025-01-21 11:30:38 +0300

    Authorization logic + razmetka no database

---
+++ KT6Iarovikov/KT6Iarovikov/Pages/LoginPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KT6Iarovikov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            try
+            {
+                if (string.IsNullOrEmpty(LoginTextBox.Text))
+                {
+                    errors.AppendLine("Заполните логин");
+                }
+
+                if (string.IsNullOrEmpty(PasswordTextBox.Password))
+                {
+                    errors.AppendLine("Заполните пароль");
+                }
+
+                if (errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                if (Data.TradeEntities.GetContext().User
+                    .Any(d => d.UserLogin == LoginTextBox.Text && 
+                    d.UserPassword == PasswordTextBox.Password))
+                {
+                    Classes.Manager.MainFrame.Navigate(new Pages.ProductPage());
+                    MessageBox.Show("Успех", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
